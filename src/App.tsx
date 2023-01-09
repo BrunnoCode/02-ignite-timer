1a16264dc00fada9285310f038fc1ee7d0ab7004
Brunno Warley <brunnowr.bw@gmail.com>
2023-01-09 01:36:56 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button.jsx'
+import { GlobalStyle } from './styles/global.js';
+import { defaultTheme } from './styles/themes/default.js';
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+      <Button />
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
+}
