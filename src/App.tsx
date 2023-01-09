3869f2bf83f2d76b3d51b02e971682d3232ea49d
Brunno Warley <brunnowr.bw@gmail.com>
2023-01-09 02:49:41 +0100

    usando eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components'
 import { Button } from './components/Button.jsx'
-import { GlobalStyle } from './styles/global.js';
-import { defaultTheme } from './styles/themes/default.js';
+import { GlobalStyle } from './styles/global.js'
+import { defaultTheme } from './styles/themes/default.js'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant='primary'/>
-      <Button variant='secondary'/>
-      <Button variant='danger'/>
-      <Button variant='success'/>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
+      <Button variant="success" />
       <Button />
 
       <GlobalStyle />
     </ThemeProvider>
-  );
+  )
 }
