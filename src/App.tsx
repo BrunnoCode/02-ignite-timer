b8cc401ce034360487e56f8d840e55e68ae97f8c
Brunno Warley <brunnowr.bw@gmail.com>
2023-01-10 02:13:55 +0100

    utilizando router

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { Button } from './components/Button.jsx'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router.js'
+
 import { GlobalStyle } from './styles/global.js'
 import { defaultTheme } from './styles/themes/default.js'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
