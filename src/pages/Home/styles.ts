164f726b6b5d5ec4e06b0f98b8cc3c52142e89fd
Brunno Warley <brunnowr.bw@gmail.com>
2023-03-02 19:23:52 +0100

    update

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
@@ -13,7 +13,7 @@ export const HomeContainer = styled.main`
     align-items: center;
     gap: 3.5rem;
   }
-`;
+`
 
 export const FormContainer = styled.div`
   width: 100%;
@@ -21,36 +21,36 @@ export const FormContainer = styled.div`
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
+  color: ${(props) => props.theme['gray-100']};
   font-size: 1.125rem;
   font-weight: bold;
   flex-wrap: wrap;
-`;
+`
 
 export const CountdownContainer = styled.div`
-  font-family: "Roboto Mono", monospace;
+  font-family: 'Roboto Mono', monospace;
   font-size: 10rem;
   line-height: 8rem;
-  color: ${(props) => props.theme["gray-100"]};
+  color: ${(props) => props.theme['gray-100']};
 
   display: flex;
   gap: 1rem;
 
   span {
-    background: ${(props) => props.theme["gray-700"]};
+    background: ${(props) => props.theme['gray-700']};
     padding: 2rem 1rem;
     border-radius: 8px;
   }
-`;
+`
 
 export const Separator = styled.div`
-  color: ${(props) => props.theme["green-500"]};
+  color: ${(props) => props.theme['green-500']};
   padding: 2rem 0;
   width: 4rem;
   overflow: hidden;
   display: flex;
   justify-content: center;
-`;
+`
 
 export const BaseCountButton = styled.button`
   width: 100%;
@@ -62,7 +62,7 @@ export const BaseCountButton = styled.button`
   align-items: center;
   justify-content: center;
   gap: 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
+  color: ${(props) => props.theme['gray-100']};
 
   font-weight: bold;
   cursor: pointer;
@@ -71,28 +71,26 @@ export const BaseCountButton = styled.button`
     opacity: 0.7;
     cursor: not-allowed;
   }
-
- 
-`;
+`
 const baseInput = styled.input`
   background: transparent;
   height: 2.5rem;
   border: 0;
-  border-bottom: 2px solid ${(props) => props.theme["gray-500"]};
+  border-bottom: 2px solid ${(props) => props.theme['gray-500']};
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
-  color: ${(props) => props.theme["gray-100"]};
+  color: ${(props) => props.theme['gray-100']};
 
   &:focus {
     box-shadow: none;
-    border-color: ${(props) => props.theme["green-500"]};
+    border-color: ${(props) => props.theme['green-500']};
   }
 
   &::placeholder {
-    color: ${(props) => props.theme["gray-500"]};
+    color: ${(props) => props.theme['gray-500']};
   }
-`;
+`
 
 export const TaskInput = styled(baseInput)`
   flex: 1;
@@ -100,26 +98,25 @@ export const TaskInput = styled(baseInput)`
   &::-webkit-calendar-picker-indicator {
     display: none !important;
   }
-`;
+`
 export const MinuteAmountInput = styled(baseInput)`
   width: 4rem;
-`;
+`
 
 export const StartCountButton = styled(BaseCountButton)`
-  background: ${(props) => props.theme["green-500"]};
-  color: ${(props) => props.theme["gray-100"]};
+  background: ${(props) => props.theme['green-500']};
+  color: ${(props) => props.theme['gray-100']};
   transition: 0.3s;
 
   &:not(:disabled):hover {
-    background: ${(props) => props.theme["green-700"]};
+    background: ${(props) => props.theme['green-700']};
   }
-
-`;
+`
 export const StopCountButton = styled(BaseCountButton)`
-   background: ${(props) => props.theme["red-500"]};
+  background: ${(props) => props.theme['red-500']};
   transition: 0.3s;
 
   &:not(:disabled):hover {
-    background: ${(props) => props.theme["red-700"]};
+    background: ${(props) => props.theme['red-700']};
   }
-`;
+`
